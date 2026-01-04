86ec7ee105460d3331b82617d95dc4584c22d91d
Akia Samuel Chah <sammi7drums@gmail.com>
2026-01-04 02:39:08 +0100

    customer order form has been created

--- src/pages/CreateOrder.jsx
+++ src/pages/CreateOrder.jsx
@@ -2,7 +2,7 @@ function CreateOrder() {
   return (
     <div className="min-h-screen bg-gray-100 p-4 flex items-center justify-center">
       <div className="w-full max-w-md bg-white rounded-xl shadow-lg p-6">
-        <h2 className="text-xl font-bold text-gray-800 mb-4">
+        <h2 className="text-xl font-bold text-center text-gray-800 mb-4">
           Create Delivery Order
         </h2>
 
